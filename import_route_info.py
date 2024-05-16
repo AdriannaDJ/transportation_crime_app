c50b70d94f114d12ae0e9dc35b1a7e5eb2c43d0e
Grant Gorham <grantegorham26@gmail.com>
2024-05-16 14:47:18 -0600

    Update import_route_info.py

--- import_route_info.py
+++ import_route_info.py
@@ -1,3 +1,4 @@
+# import dependencies
 from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
